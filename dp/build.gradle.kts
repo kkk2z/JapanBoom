b7fe00f7004f8a0d41c006b84f32cc903c241d62
kkk2z <139834827+kkk2z@users.noreply.github.com>
2024-09-18 02:30:47 +0000

    自作プラグイン....

---
+++ dp/build.gradle.kts
@@ -0,0 +1,14 @@
+buildscript {
+    ext {
+        kotlin_version = '1.9.20'
+        spigot_version = '1.20.2-R0.1-SNAPSHOT'
+    }
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20"
+    }
+}
+
+// 下に色んな文がある
