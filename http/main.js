0f7f2a5cab3e408a16218c3484f98819a63cd036
kkk2z <139834827+kkk2z@users.noreply.github.com>
2024-10-12 10:20:46 +0000

    あ

---
+++ http/main.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const port = 3000;
+
+const server = http.createServer((request, response) => {
+    response.writeHead(200, {
+      "Content-Type": "text/html"
+    });
+
+    const responseMessage = "<h1>Hello World</h1>";
+    response.end(responseMessage);
+    console.log(`Sent a response : ${responseMessage}`);
+});
+
+server.listen(port);
+console.log(`The server has started and is listening on port number: ${port}`);
